0277486fcbce6e9d37c2640704407c22dd8e6bdf
Gilbert Brault <gbrault@seadev.org>
2020-09-14 19:51:48 +0200

    Update __init__.py

--- pysketcher/__init__.py
+++ pysketcher/__init__.py
@@ -6,8 +6,6 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-#from future import standard_library
-#standard_library.install_aliases()
 from builtins import *
 __version__ = '0.2'
 __author__ = 'Hans Petter Langtangen <hpl@simula.no>, Gilbert Brault <gbrault@seadev.org>'
